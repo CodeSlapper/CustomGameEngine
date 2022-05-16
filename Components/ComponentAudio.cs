b6483c7c91f8359f9be822eea5b0aa0fc473d680
C.A <81365716+CodeSlapper@users.noreply.github.com>
2022-05-16 18:14:57 +0300

    Add files via upload

---
+++ Components/ComponentAudio.cs
@@ -0,0 +1,45 @@
+﻿using OpenGL_Game.Managers;
+using OpenGL_Game.OBJLoader;
+using System.IO;
+using OpenTK.Audio.OpenAL;
+using OpenTK;
+
+namespace OpenGL_Game.Components
+{
+    class ComponentAudio : IComponent
+    {
+        int audioSource;
+        public ComponentAudio (string audioname)
+        {
+            int audioBuffer;
+            
+            // NEW for Audio
+            // Setup Audio Source from the Audio Buffer
+            audioBuffer = ResourceManager.LoadAudio(audioname);
+            audioSource = AL.GenSource();
+            AL.Source(audioSource, ALSourcei.Buffer, audioBuffer); // attach the buffer to a source
+            AL.Source(audioSource, ALSourceb.Looping, true); // source loops infinitely
+           // sourcePosition = new Vector3(10.0f, 0.0f, 0.0f); // give the source a position
+           
+            AL.SourcePlay(audioSource); // play the ausio source
+        }
+        //need public that sets position for the audio (method)  called for system
+        public void SetPosition(Vector3 emitterPosition)
+        {
+
+            AL.Source(audioSource, ALSource3f.Position, ref emitterPosition);
+        }
+        
+
+        public ComponentTypes ComponentType
+        {
+            get { return ComponentTypes.COMPONENT_AUDIO; }
+        }
+        public void Close()
+        {
+            AL.SourceStop(audioSource);
+            AL.DeleteSource(audioSource);
+        }
+
+    }
+}
---
+++ Components/ComponentCollisionBox.cs
@@ -0,0 +1,52 @@
+﻿using OpenGL_Game.Systems;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenGL_Game.Components
+{
+    class ComponentCollisionBox : IComponent
+    {
+        double top;
+        double bottom;
+        double left;
+        double right;
+        public Box2d Box;
+        public ComponentCollisionBox(double left, double top, double right, double bottom)
+        {
+            this.Box = new Box2d(left, top, right, bottom);
+            this.top = top;
+            this.bottom = bottom;
+            this.left = left;
+            this.right = right;
+        }
+        public double Top
+        {
+            get { return top; }
+            set { top = value; }
+        }
+        public double Bottom
+        {
+            get { return bottom; }
+            set { bottom = value; }
+        }
+        public double Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        public double Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+        
+        public ComponentTypes ComponentType
+        {
+            get { return ComponentTypes.COMPONENT_COLLISION_BOX; }
+        }
+    }
+}
+
